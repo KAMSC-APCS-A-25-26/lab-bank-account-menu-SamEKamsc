4bd0e5a5d4aac6245e351e16c3bf9cc093200e9f
SamEKamsc <03samuel.edmonds@kamsc.org>
2025-09-22 14:00:12 -0400

    Copied the code from paper

--- src/main/java/BankAccountMenu.java
+++ src/main/java/BankAccountMenu.java
@@ -1,13 +1,56 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class BankAccountMenu {
     public static void main(String[] args) {
-        // TODO: Implement the bank account menu
-        // 1. Create a double variable for balance
-        // 2. Create a while loop for the menu
-        // 3. Display the menu options
-        // 4. Use Scanner to read user input
-        // 5. Use switch statement to handle menu choices
-        // 6. Implement add money, withdraw money, check balance, and exit functionality
+        Scanner sc = new Scanner(System.in);
+        double balance = 0.0;
+        boolean run = true;
+        while (run) {
+            System.out.println("--- Banl Account Menu ---");
+            System.out.println("1. Add Money");
+            System.out.println("2. Withdraw Money");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice:  ");
+            int choice = sc.nextInt();
+            System.out.println();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter amount to Add:  ");
+                    double add = sc.nextDouble();
+                    System.out.println();
+                    if (add >0) {
+                        System.out.println("Added $" + (Math.round(add*100))/100.0);
+                        balance += (Math.round(balance*100))/100.0;
+                        System.out.println("New balance: $" + (Math.round(balance*100))/100.0);
+                    }
+                    else {
+                        System.out.println("Enter positive number");
+                    }
+                    break;
+                case 2:
+                    System.out.print("Enter amount to withdraw:  ");
+                    double withdraw = sc.nextDouble();
+                    System.out.println();
+                    if (withdraw > 0 && withdraw <= balance) {
+                        balance -= (Math.round(withdraw*100))/100.0;
+                        System.out.println("Withdrew $" + (Math.round(balance*100))/100.0);
+                        System.out.println("New balance: $" + (Math.round(balance*100))/100.0);
+                    }
+                    else {
+                        System.out.println("Insufficient funds");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Balance: $" + (Math.round(balance*100))/100.0);
+                    break;
+                case 4:
+                    System.out.println("Goodbye!");
+                    run = false;
+                    break;
+                    
+            }
+        }
     }
 }
